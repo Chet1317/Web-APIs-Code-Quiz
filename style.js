e0a8be494d11f2e73ae0ef1a1b41f63cc536653b
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-05-28 13:22:07 -0400

    created new variables and id's in order to insert answers for first question when pressing start

--- style.js
+++ style.js
@@ -1,22 +1,33 @@
 const startButton = document.getElementById("start-btn")
-const questionBox = document.getElementById("question-container")
-
+//const answerButtons = document.getElementById("answer-buttons") 
 startButton.addEventListener('click', startgame)
+var counter = 0
+
 
 function startgame (){
+   var questionEl = document.getElementById("question") 
+   questionEl.innerText = questionElement[counter].question
+   var answerEl = document.getElementById("answer-1")
+   answerEl.innerHTML = questionElement[counter].answer[0].text
+   answerEl = document.getElementById("answer-2")
+   answerEl.innerHTML = questionElement[counter].answer[1].text
     console.log("start game")
-   questionBox.classList.remove("hide")
+    counter++
+
 }
 
 function nextquestion (){
-
+   
 }
-
+function showQuestion (){
+   
+    console.long("question is shown")
+}
 function answer (){
 
 }
 
-const questions = [
+const questionElement = [
     {
         question: "Who invented Javascript?",
         answer: [
