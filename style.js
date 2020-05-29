183d7a78e94c03f5069b9aef0ff843756fef57c4
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-05-29 13:02:30 -0400

    had to make a few changes to the start quiz function and next questions function in order to get the quiz to work properply according to the rrequirements

--- style.js
+++ style.js
@@ -1,10 +1,14 @@
 const startButton = document.getElementById("start-btn")
 //const answerButtons = document.getElementById("answer-buttons") 
 startButton.addEventListener('click', startgame)
+const nextanswer1 = document.getElementById("answer-1")
+const nextanswer2 = document.getElementById("answer-2")
+nextanswer1.addEventListener("click", nextquestion)
+nextanswer2.addEventListener("click", nextquestion)
 var counter = 0
 
-
 function startgame (){
+   
    var questionEl = document.getElementById("question") 
    questionEl.innerText = questionElement[counter].question
    var answerEl = document.getElementById("answer-1")
@@ -15,18 +19,32 @@ function startgame (){
    answerEl.innerHTML = questionElement[counter].answer[2].text
    answerEl = document.getElementById("answer-4")
    answerEl.innerHTML = questionElement[counter].answer[3].text
+  
     console.log("start game")
-    counter++
 
 }
 
 function nextquestion (){
-   
+    
+    var questionEl = document.getElementById("question") 
+    questionEl.innerText = questionElement[counter].question
+    var answerEl = document.getElementById("answer-1")
+    answerEl.innerHTML = questionElement[counter].answer[0].text
+    answerEl = document.getElementById("answer-2")
+    answerEl.innerHTML = questionElement[counter].answer[1].text
+    answerEl = document.getElementById("answer-3")
+    answerEl.innerHTML = questionElement[counter].answer[2].text
+    answerEl = document.getElementById("answer-4")
+    answerEl.innerHTML = questionElement[counter].answer[3].text
+    
+    
+    counter++
 }
-function showQuestion (){
+
+//function nextquestion (){
    
-    console.long("question is shown")
-}
+   
+
 function answer (){
 
 }
@@ -40,13 +58,37 @@ const questionElement = [
             {text: "Donald Trump", correct: false},
             {text: "Steve Jobs", correct: false}]
     },
-    {
-        question: "Who invented Javascript?",
+    
+    {question: "JavaScript was invented in what year?",
         answer: [
-            {text: "Brendan Eich", correct: true},
+            {text: "1986", correct: true},
             {text: "Bill Gates", correct: false},
             {text: "Donald Trump", correct: false},
             {text: "Steve Jobs", correct: false}]
-    }
+        },
+        {question: "What is a paramater",
+        answer: [
+            {text: "Used in a function", correct: true},
+            {text: "Bill Gates", correct: false},
+            {text: "Donald Trump", correct: false},
+            {text: "Steve Jobs", correct: false}]
+        }
 
+        ,
+        {question: "What is a paramater",
+        answer: [
+            {text: "Used in a function", correct: true},
+            {text: "Bill Gates", correct: false},
+            {text: "Donald Trump", correct: false},
+            {text: "Steve Jobs", correct: false}]
+        }
+
+        ,
+        {question: "What is a $ in Java",
+        answer: [
+            {text: "Used in a function", correct: true},
+            {text: "Bill Gates", correct: false},
+            {text: "Donald Trump", correct: false},
+            {text: "Steve Jobs", correct: false}]
+        }
 ]
